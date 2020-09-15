7556d114eeed7c48e0e439228aa7a5e87a98e9a1
williamsjs <williamsjs1224@gmail.com>
2020-09-15 08:31:19 -0400

    Add NewQuestion component placeholder

---
+++ src/components/NewQuestion/NewQuestion.js
@@ -0,0 +1,15 @@
+class NewQuestion extends Component {
+  state = { 
+
+  }
+
+  render() { 
+    return ( 
+      <div className="new-question">
+        New Question
+      </div>
+    );
+  }
+}
+ 
+export default NewQuestion;
